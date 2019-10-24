0cee7f91f05f2a5a6c727f541c7e90c9cf57d274
evansavage <esav678@gmail.com>
2019-10-24 14:07:19 -0400

    Included interface for selecting specific update files and corresponding directories

--- presentation_update.py
+++ presentation_update.py
@@ -2,10 +2,16 @@ from bs4 import BeautifulSoup
 import re
 import os
 
-if not os.path.isdir('_presentations'):
-    os.mkdir('_presentations')
+
+print('What directory would you like to update?\n  - publications\n  - presentations\n  - media')
+update_direc = input()
+
+if not os.path.isdir(f'_{ update_direc }'):
+    os.mkdir(f'_{ update_direc }')
 
 print('What zotero export file (HTML) would you like to parse?')
+for file in os.listdir('zotero_export'):
+    print(f'  - { file }')
 export_file = input()
 
 with open(f'zotero_export/{ export_file }', "r") as f:
@@ -17,8 +23,8 @@ with open(f'zotero_export/{ export_file }', "r") as f:
         year = re.search(r'\d{4}', text).group(0)
         author = text.split(", ")[0]
         title = re.search(r'“(.*?)”', text).group(0).strip('“').strip('”')[:-1].replace(" ", "_")
-        if not os.path.isdir(f'_presentations/{ year }'):
-            os.mkdir(f'_presentations/{ year }')
-        with open(f'_presentations/{ year }/{ author }_{ title }_{ year }.md', 'w') as p:
+        if not os.path.isdir(f'_{ update_direc }/{ year }'):
+            os.mkdir(f'_{ update_direc }/{ year }')
+        with open(f'_{ update_direc }/{ year }/{ author }_{ title }_{ year }.md', 'w') as p:
             p.write(f'---\npresentation_year: { year }\n---\n{ text }\n')
         # print(text, year, author, title)
