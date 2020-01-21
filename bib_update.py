cf3cf448b2ecb318fc29abd4f3220de2ca753c63
evansavage <esav678@gmail.com>
2020-01-21 16:11:50 -0500

    Updated css to sass for more useful formatting

--- bib_update.py
+++ bib_update.py
@@ -19,12 +19,13 @@ with open(f'zotero_export/{ export_file }', "r") as f:
     soup = BeautifulSoup(contents, 'lxml')
     divs = soup.find_all('div',{"class": "csl-entry"})
     for div in divs:
+        print('yes')
         text = div.text
         year = re.search(r'\d{4}', text).group(0)
         author = text.split(", ")[0]
         title = re.search(r'“(.*?)”', text).group(0).strip('“').strip('”')[:-1].replace(" ", "_")
         if not os.path.isdir(f'_{ update_direc }/{ year }'):
             os.mkdir(f'_{ update_direc }/{ year }')
-        with open(f'_{ update_direc }/{ year }/{ author }_{ title }_{ year }.md', 'w') as p:
+        with open(f'_{ update_direc }/{ year }/{ author[:30] }_{ title[:25] }_{ year }.md', 'w') as p:
             p.write(f'---\npresentation_year: { year }\n---\n{ text }\n')
         # print(text, year, author, title)
